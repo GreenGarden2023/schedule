a19b7b5a61f8d29ab091ae9a0f61b8139ae02c2f
Tran Ty Go <gott150899@gmail.com>
2023-06-03 01:38:46 +0700

    fix bug

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const httpClient = require('./lib/http-client');
 const app = express(); 
 const port = process.env.PORT || 8080
 
-testFunction()
+// testFunction()
 
 app.get('/', function(req, res){
     // testFunction()
     res.send("Hello World");
 })
-// app.get('/cron-job', async (req, res) =>{
-//     console.log('start job')
-    
-//     testFunction()
-//     res.send('started cron job')
-// })
+app.get('/cron-job', async (req, res) =>{
+    testFunction()
+    res.send('started cron job')
+})
 
 app.listen(port, function(){
     console.log("Your app running on port " + port);
